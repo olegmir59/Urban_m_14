db36654cb798b20e0ead896b00414de80f349e3d
olegmir59 <udom1@ya.ru>
2024-10-25 21:42:41 +0700

    Add files

---
+++ module_14_1.py
@@ -0,0 +1,38 @@
+#  Домашнее задание по теме "Создание БД, добавление, выбор и удаление элементов."
+# Цель: освоить основные команды языка SQL и использовать их в коде используя SQLite3.
+#
+# Задача "Первые пользователи":
+
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age  TEXT NOT NULL,
+balance TEXT NOT NULL
+)
+''')
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+
+#for i in range(1, 11):
+#    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f"User{i}", f"example{i}@gmail.com", i*10, 1000))
+
+#for i in range(1, 11, 2):
+#    cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"User{i}"))
+#for i in range(1, 11, 3):
+#    cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i}",))
+
+cursor.execute("SELECT * FROM Users WHERE NOT age = ?", (60,))
+users = cursor.fetchall()
+for user in users:
+    print(f"Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}")
+
+
+
+connection.commit()
+connection.close()
